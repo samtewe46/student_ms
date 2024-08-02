5866ff64695fc99d95af3224f803d1cb84643045
Samuel Tewelde <samueltewelde@Samuels-MacBook-Pro.local>
2024-08-02 05:24:49 -0700

    implemeted crud operation

--- src/main/java/com/student_ms/student/ms/service/StudentService.java
+++ src/main/java/com/student_ms/student/ms/service/StudentService.java
@@ -2,6 +2,21 @@ package com.student_ms.student.ms.service;
 
 import com.student_ms.student.ms.entity.Student;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentService {
     Student save( Student student);
+
+    List<Student> getStudentByName(String name);
+
+    List<Student> getAllStudents();
+
+    Optional<Student> getStudentById(Long id);
+
+    List<Student> getStudentByGender(String gender);
+
+    Student updateStudentById(Long id, Student student);
+
+    void deleteStudentById(Long id);
 }
--- src/main/java/com/student_ms/student/ms/service/StudentServiceImpl.java
+++ src/main/java/com/student_ms/student/ms/service/StudentServiceImpl.java
@@ -3,7 +3,9 @@ package com.student_ms.student.ms.service;
 import com.student_ms.student.ms.entity.Student;
 import com.student_ms.student.ms.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class StudentServiceImpl implements StudentService {
@@ -15,4 +17,46 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.save(student);
     }
 
+    @Override
+    public List<Student> getStudentByName(String name) {
+        return studentRepository.getStudentByName(name);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    @Override
+    public List<Student> getStudentByGender(String gender) {
+        return studentRepository.getStudentByGender(gender);
+    }
+
+    @Override
+    public Student updateStudentById(Long id, Student studentDetails) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (studentOptional.isPresent()) {
+            Student student = studentOptional.get();
+            student.setName(studentDetails.getName());
+            student.setGender(studentDetails.getGender());
+            studentRepository.save(student);
+
+        }
+        return null;
+
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+
 }
+
+
